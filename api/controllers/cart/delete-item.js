4c79245188befdd76b1fea4f838dfc283e6a84c6
gagana <gagana@codevus.com>
2021-09-30 01:21:16 +0530

    customers orders

--- api/controllers/cart/delete-item.js
+++ api/controllers/cart/delete-item.js
@@ -20,6 +20,16 @@ module.exports = {
   fn: async function (inputs, exits) {
     await CartItems.destroy({ id: inputs.item });
 
+    var inventory = await Inventory.findOne({ id: cart_item.item_id });
+
+    var new_quantity = inventory.quantity + cart_item.quantity;
+
+    await Inventory.update({ id: inventory.id }).set({
+      quantity: new_quantity,
+    });
+
+    await CartItems.destroy({ id: inputs.item });
+
     return exits.success({
       status: true,
     });
